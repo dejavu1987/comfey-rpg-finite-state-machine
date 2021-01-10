bd990fb254b0af4e9b2e95d6cc62fef0b52ed9fa
Anil Maharjan <anil@anilmaharjan.com.np>
2021-01-10 20:36:45 +0100

    animation

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const DEBUG = false;
 const app = new Comfey(document.getElementById("app"), DEBUG);
 const $buttons = document.getElementById("buttons");
 const $inventory = document.getElementById("inventory");
+const $cashChange = document.getElementById("cash-update");
 // Initialize states
 const [, setLocation] = app.useState(
   "location",
@@ -15,7 +16,7 @@ const [, setLocation] = app.useState(
   locationWatcher
 );
 
-const [getCash, setCash] = app.useState("cash", 500);
+const [getCash, setCash] = app.useState("cash", 500, cashWatcher);
 const [getInventory, setInventory] = app.useState(
   "inventory",
   [],
@@ -23,6 +24,18 @@ const [getInventory, setInventory] = app.useState(
 );
 
 setLocation(journey.start);
+
+// Watchers
+function cashWatcher(newCash, oldCash) {
+  $cashChange.innerText = newCash - oldCash;
+  $cashChange.classList.remove("animate__fadeOutUp");
+  $cashChange.classList.add("animate__fadeInUp");
+
+  setTimeout(() => {
+    $cashChange.classList.remove("animate__fadeInUp");
+    $cashChange.classList.add("animate__fadeOutUp");
+  }, 800);
+}
 
 function inventoryWatcher(newInv) {
   updateInventory(newInv);
@@ -32,6 +45,7 @@ function locationWatcher(newLoc, oldLoc) {
   updateButtons(newLoc);
 }
 
+// UI Updates
 function updateInventory(inventory) {
   $inventory.innerHTML = inventory
     .sort()
@@ -55,25 +69,6 @@ function updateButtons(location) {
       }
     }
   }
-}
-
-function actionButtonClick(e) {
-  const target = e.target;
-  if (target.dataset.cash) {
-    setCash(getCash() + parseInt(target.dataset.cash));
-  }
-  if (target.dataset.inventory) {
-    if (target.dataset.inventory.substring(0, 1) === "+") {
-      const item = target.dataset.inventory.replace("+", "");
-
-      setInventory([...getInventory(), item]);
-    }
-  }
-}
-
-function moveButtonClick(e) {
-  const target = e.target;
-  setLocation(target.dataset.location);
 }
 
 function addMoveButton(action, location) {
@@ -100,4 +95,24 @@ function addActionButton(action, results) {
     }
   }
   $buttons.appendChild(btn);
+}
+
+// Button handlers
+function actionButtonClick(e) {
+  const target = e.target;
+  if (target.dataset.cash) {
+    setCash(getCash() + parseInt(target.dataset.cash));
+  }
+  if (target.dataset.inventory) {
+    if (target.dataset.inventory.substring(0, 1) === "+") {
+      const item = target.dataset.inventory.replace("+", "");
+
+      setInventory([...getInventory(), item]);
+    }
+  }
+}
+
+function moveButtonClick(e) {
+  const target = e.target;
+  setLocation(target.dataset.location);
 }
